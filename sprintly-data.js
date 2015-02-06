4a77a4e853eda14a2d45d6d930c0baf7d62df5e2
Sam Breed <sam@quickleft.com>
2015-02-06 11:41:09 -0800

    add support for bearer auth

--- sprintly-data.js
+++ sprintly-data.js
@@ -3,10 +3,16 @@ import Backbone from './lib/support/backbone';
 import Products from './lib/products';
 import collectionConsumer from './lib/page_consumer';
 import basicAuth from './lib/support/basic-auth';
+import bearerAuth from './lib/support/bearer-auth';
 import { VERSION } from './package.json';
 
 function Client(email, apiKey) {
-  Backbone.ajax = basicAuth(email, apiKey);
+  if (typeof email !== 'string' && email.token) {
+    Backbone.ajax = bearerAuth(email.token);
+  } else {
+    Backbone.ajax = basicAuth(email, apiKey);
+  }
+
   this.products = new Products();
   this.user = new User();
 }
